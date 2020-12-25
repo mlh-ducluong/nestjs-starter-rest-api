ef17f9bed580e82d1d304f9fb6447d48c21e7078
mlh-ducluong <58024202+mlh-ducluong@users.noreply.github.com>
2020-12-25 16:02:20 +0900

    Initial commit

---
+++ src/auth/dtos/auth-register-input.dto.ts
@@ -0,0 +1,43 @@
+import {
+  IsNotEmpty,
+  MaxLength,
+  Length,
+  IsString,
+  IsArray,
+  IsEnum,
+  IsOptional,
+  ArrayUnique,
+  IsEmail,
+  IsBoolean,
+} from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { ROLE } from '../constants/role.constant';
+
+export class RegisterInput {
+  @ApiProperty()
+  @IsNotEmpty()
+  @MaxLength(100)
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @MaxLength(200)
+  @IsString()
+  username: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @Length(6, 100)
+  @IsString()
+  password: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsEmail()
+  @MaxLength(100)
+  email: string;
+
+  // These keys can only be set by ADMIN user.
+  roles: ROLE[] = [ROLE.USER];
+  isAccountDisabled: boolean;
+}
